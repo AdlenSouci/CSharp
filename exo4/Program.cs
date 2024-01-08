b3b4a7bf61af8abdff437cab9192fa9afe528649
adlen souci <77266719+AdlenSouci@users.noreply.github.com>
2024-01-08 14:48:25 +0100

    exo 3

---
+++ exo4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Hello DONNE TON PRENOM");
+string prenom = Console.ReadLine();
+Console.WriteLine("Hello DONNE TON age");
+string age = Console.ReadLine();
+
+string texte = $"Bonjour {prenom}, tu as {age} ans";
+
+Console.WriteLine(texte);
