c6bd8fe5a4414d8504e7f443d9c8f67dda084c49
adlen souci <77266719+AdlenSouci@users.noreply.github.com>
2024-01-08 14:52:18 +0100

    exo 3  avec modificationn

--- exo4/Program.cs
+++ exo4/Program.cs
@@ -6,6 +6,17 @@ Console.WriteLine("Hello DONNE TON PRENOM");
 string prenom = Console.ReadLine();
 Console.WriteLine("Hello DONNE TON age");
 string age = Console.ReadLine();
+Console.WriteLine("Hello DONNE Ta couleur exemple red");
+string couleur = Console.ReadLine();
+if (couleur == "red")
+
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+}
+else
+{
+    Console.BackgroundColor = ConsoleColor.Blue;
+}
 
 string texte = $"Bonjour {prenom}, tu as {age} ans";
 
--- exo4/obj/Debug/net8.0/exo4.AssemblyInfo.cs
+++ exo4/obj/Debug/net8.0/exo4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exo4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17314d9aac2c6f33c5383b4ab60f8dae68358a76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b4a7bf61af8abdff437cab9192fa9afe528649")]
 [assembly: System.Reflection.AssemblyProductAttribute("exo4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exo4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
