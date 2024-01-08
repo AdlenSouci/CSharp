06b608af72e57243205edc2ab201f27333a302f2
adlen souci <77266719+AdlenSouci@users.noreply.github.com>
2024-01-08 15:54:09 +0100

    exo 4  avec modificationn

---
+++ exo5/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("conversions");
+
+int i = 42;
+string s = i.ToString();
+
+Console.WriteLine(s);
+
+string s2 = "1234";
+//convertir une chaine de caractère en entier
+int i2 = int.Parse(s2);
+
+string chaine123 = "123";
+int i123 = Convert.ToInt32(chaine123);
+
+Console.WriteLine("entrer votre age :");
+string input = Console.ReadLine();
+int age= int.Parse(input);
+int annee = 2024 - age;
+Console.WriteLine($"votre annees de naissance : {annee}");
+
+
