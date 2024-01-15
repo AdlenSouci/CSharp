2071db435156f869de205f87ea733b827d69c3f0
adlen souci <77266719+AdlenSouci@users.noreply.github.com>
2024-01-15 17:03:07 +0100

    exo 7  avec modificationn

---
+++ exo7/Program.cs
@@ -0,0 +1,30 @@
+﻿//equation du second degre
+console.Write("entrer valeur de a : ");
+int a = int.Parse(Console.ReadLine());
+console.Write("entrer valeur de b : ");
+int b = int.Parse(Console.ReadLine());
+console.Write("entrer valeur de c : ");
+int c = int.Parse(Console.ReadLine());
+
+
+double discriminant = Math.Pow(b,2) - 4*a*c; // discriminant = b*b - 4*a*c;
+
+
+if (discriminant > 0)
+{
+    double racinePos = (-b + Math.Sqrt(discriminant)) / (2*a);
+    double racineNeg = (-b - Math.Sqrt(discriminant)) / (2*a);
+    Console.WriteLine("les solutions de l'equation sont : {racinePos} et  {racineNeg}");
+}
+else if (discriminant == 0)
+{
+    double racineUnique = -b / (2*a);
+    Console.WriteLine($"l'equation a une seule solution : {racineUnique}");
+}
+else
+{
+    Console.WriteLine("l'equation n'a pas de solution");
+}
+
+Console.ReadLine();
+
