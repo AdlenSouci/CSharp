f7b673a006736fbf0f672bd8394f111ea5ab9193
adlen souci <77266719+AdlenSouci@users.noreply.github.com>
2024-01-15 17:03:21 +0100

    exo 7

--- exo6/Program.cs
+++ exo6/Program.cs
@@ -1,31 +1,31 @@
-﻿Console.WriteLine($"deviner le nombre mystere");
+﻿Console.WriteLine("deviner le nombre mystere");
 
-Random random = new Random();
-int nombreMystere = random.Next(1, 100);
+Random rdom = new Random();
+int nombreMystere = rdom.Next(1, 100);
 int nombreUtilisateur;
 int essai = 0;
 
 while (essai < 5)
 {
-    Console.WriteLine($"entrer un nombre entre 1 et 100");
+    Console.WriteLine("entrer un nombre entre 1 et 100");
     nombreUtilisateur = int.Parse(Console.ReadLine());
     if (nombreUtilisateur == nombreMystere)
     {
-        Console.WriteLine($"bravo vous avez gagner");
+        Console.WriteLine("bravo vous avez gagner");
         break;  //sortir de la boucle
     }
     else if (nombreUtilisateur < nombreMystere)
     {
-        Console.WriteLine($"le nombre mystere est plus grand");
+        Console.WriteLine("le nombre mystere est plus grand");
     }
     else
     {
-        Console.WriteLine($"le nombre mystere est plus petit");
+        Console.WriteLine("le nombre mystere est plus petit");
     }
     essai++;
     if (essai == 5)
     {
-        Console.WriteLine($"vous avez perdu, le nombre mystere est {nombreMystere}");
+        Console.WriteLine("vous avez perdu, le nombre mystere est {nombreMystere}");
     }
 
 }
