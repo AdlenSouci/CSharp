4995f12ba0b56a6d8d5b49b43e6a8283a9bb079a
adlen souci <77266719+AdlenSouci@users.noreply.github.com>
2024-01-22 11:36:23 +0100

    new exo poo

---
+++ POO/MaClasse.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace POO
+{
+    public class MaClasse
+    {
+
+        public void MaMethode()
+        {
+            Console.WriteLine("MaMethode");
+        }
+
+        public string Ecrire()
+        {
+            Console.WriteLine("Ecrire");
+           
+        }
+
+        public string Lire()
+        {
+           return Console.ReadLine();
+        }
+       
+
+
+    }
+
+
+}
---
+++ POO/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO;
+
+Console.WriteLine("Hello, classes");
+MaClasse maClasse = new MaClasse();
+maClasse.MaMethode();
